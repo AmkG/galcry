666086fe218952e7a1ba87edb99866e88f35c4e8
Alan Manuel K. Gloria <almkglor@gmail.com>
2013-06-30 08:50:45 +0800

    src/backend/main.c: Add basic command loop.
      Not yet complete, need to parse command line.

--- src/backend/main.c
+++ src/backend/main.c
@@ -17,8 +17,107 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include"all_config.h"
 
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Command_s
+{
+  char const *command;
+  void (*handler) (int argc, char const **argv);
+};
+typedef struct Command_s Command;
+
+static void quit_command (int argc, char const **argv);
+static bool quit_flag;
+static int quit_value;
+
+Command command_list[] = {
+  {"quit", &quit_command},
+  {0, 0}
+};
+
+static void command_loop (void);
+
 int
 main (int argc, char **argv)
 {
-  return 0;
+  quit_flag = false;
+  quit_value = 0;
+
+  command_loop ();
+
+  return quit_value;
+}
+
+static void
+quit_command (int argc, char const **argv)
+{
+  quit_flag = true;
+}
+
+static char *
+get_line (void)
+{
+  char *rv;
+  unsigned int capacity;
+  unsigned int line_length;
+  int c;
+
+  rv = 0;
+  capacity = 0;
+  line_length = 0;
+
+  for (;;)
+    {
+      c = getchar ();
+      if (c == EOF && line_length == 0)
+	return 0;
+
+      if (line_length + 1 >= capacity)
+	{
+	  unsigned int ncap = capacity + (capacity / 2) + 4;
+	  char *nrv = malloc (ncap);
+	  if (!nrv)
+	    abort ();
+	  if (rv)
+	    memcpy (nrv, rv, line_length);
+	  free (rv);
+	  capacity = ncap;
+	  rv = nrv;
+	}
+
+      if (c == '\n' || c == EOF)
+	{
+	  rv[line_length] = 0;
+	  break;
+	}
+      else
+	{
+	  rv[line_length] = (char) c;
+	  ++line_length;
+	}
+    }
+
+  return rv;
+}
+
+static void
+command_loop (void)
+{
+  char *cmd;
+  while (!quit_flag)
+    {
+      printf ("galcry> ");
+      cmd = get_line ();
+      if (!cmd)
+	{
+	  quit_flag = true;
+	  return;
+	}
+
+      /* Divide into words.  */
+      /* Search command list.  */
+
+      free (cmd);
+    }
 }
