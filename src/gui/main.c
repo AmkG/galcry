a1d58358951c7ae36a561dda160489531612a7e6
Alan Manuel K. Gloria <almkglor@gmail.com>
2013-07-13 07:04:12 +0800

    src/gui/main.c: Try out loop framework.

--- src/gui/main.c
+++ src/gui/main.c
@@ -18,10 +18,79 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include"all_config.h"
 
 #include"cmdline.h"
+#include"gui/loop.h"
 #include"gui/utils.h"
 #include"ustr.h"
 
 #include<allegro.h>
+
+/* Simple GUI testing for loop.  */
+typedef struct SIMPLE_s
+{
+  unsigned int mx;
+  unsigned int my;
+  bool quit_flag;
+  unsigned int x;
+  unsigned int y;
+} SIMPLE;
+
+static void
+simple_input (void *vs)
+{
+  SIMPLE *s;
+
+  s = vs;
+
+  poll_mouse ();
+  s->mx = mouse_x;
+  s->my = mouse_y;
+
+  if (keypressed ())
+    {
+      (void) readkey ();
+      s->quit_flag = true;
+    }
+  else if (mouse_b & 1 || mouse_b & 2 || mouse_b & 4)
+    {
+      s->quit_flag = true;
+    }
+}
+static LOOPRETURN
+simple_logic (void *vs)
+{
+  SIMPLE *s;
+
+  s = vs;
+
+  if (s->quit_flag)
+    return LOOP_STOP;
+
+  if (s->mx != s->x || s->my != s->y)
+    {
+      s->x = s->mx;
+      s->y = s->my;
+      return LOOP_REFRESH;
+    }
+  else
+    return LOOP_CONTINUE;
+}
+static void
+simple_refresh (BITMAP *target, void const *vs)
+{
+  SIMPLE const *s;
+  int white;
+
+  set_color_depth (bitmap_color_depth (target));
+  s = vs;
+  white = makecol (255, 255, 255);
+
+  clear_to_color (target, makecol (0,0,0));
+
+  fastline (target, 0, 0, s->x, s->y, white);
+  fastline (target, res_width - 1, 0, s->x, s->y, white);
+  fastline (target, 0, res_height - 1, s->x, s->y, white);
+  fastline (target, res_width - 1, res_height - 1, s->x, s->y, white);
+}
 
 int
 main (int argc, char **argv)
@@ -38,6 +107,9 @@ main (int argc, char **argv)
       cmdline_deinit ();
       exit (1);
     }
+  (void) install_keyboard ();
+  (void) install_mouse ();
+  (void) install_timer ();
 
   if (get_desktop_resolution (&res_width, &res_height) != 0)
     {
@@ -63,6 +135,14 @@ main (int argc, char **argv)
       cmdline_deinit ();
       exit (1);
     }
+
+  {
+    SIMPLE s;
+    s.quit_flag = false;
+    s.x = 0;
+    s.y = 0;
+    loop_execute (&s, simple_refresh, simple_input, simple_logic);
+  }
 
   gui_exit (0);
 }
