ebe8fb4cc6497a77083c66b5c4ce42d42447f092
Alan Manuel K. Gloria <almkglor@gmail.com>
2013-07-13 06:27:25 +0800

    inc/gui/loop.h: Change loop API to simplify programming menus.

--- inc/gui/loop.h
+++ inc/gui/loop.h
@@ -33,15 +33,44 @@ typedef enum LOOPRETURN_e
 
 /* Set the ticks per second (number of times logic routine is called
    per second).  */
-void loop_ticks_per_second (unsigned int i);
+void loop_set_ticks_per_second (unsigned int i);
+unsigned int loop_get_ticks_per_second (void);
 /* Sets whether refresh routines should be given a video bitmap or
    a memory bitmap.  */
 void loop_set_direct_video_refresh (int setting);
 int loop_get_direct_video_refresh (void);
+
+/* Used by game loop functions.  */
+typedef void (*LoopRefresh) (BITMAP *buffer, void const *state);
+typedef void (*LoopInput) (void *state);
+typedef LOOPRETURN (*LoopLogic) (void *state);
+
 /* Execute a game loop.  Game loops may be nested by the logic
-   routine (i.e. a game loop logic routine).  */
+   routine (i.e. a game loop logic routine).
+   INPUT is expected to only check inputs (keyboard, mouse, etc.)
+   and set flags in the game state, while LOGIC is expected to
+   modify the game state based on the input and clear any input
+   flags.  The reason for separating them has to do with
+   loop_subexecute below.  */
 void loop_execute (void *state,
-		   void (*refresh) (BITMAP *buffer, void const *state),
-		   LOOPRETURN (*logic) (void *state));
+		   LoopRefresh refresh,
+		   LoopInput input,
+		   LoopLogic logic);
+/* Like loop_execute, except that the REFRESH and LOGIC of
+   higher calls to loop_subexecute or loop_execute are
+   also executed.
+   Basically, loop_subexecute puts the REFRESH and LOGIC
+   of any current loop execution onto a stack.  REFRESH
+   and LOGIC of earlier calls is called first before REFRESH
+   of later calls; the latest LOGIC controls whether or not
+   to stop, and if any LOGIC requests a refresh, all
+   REFRESH routines are called.
+   Only the latest INPUT routine is called.
+   The main purpose of this API is to allow a sub-menu to
+   capture control of the mouse and/or keyboard.  */
+void loop_subexecute (void *state,
+		      LoopRefresh refresh,
+		      LoopInput input,
+		      LoopLogic logic);
 
 #endif /* !defined (GUI_LOOP_H_INCLUDED) */
--- src/gui/loop.c
+++ src/gui/loop.c
@@ -20,11 +20,24 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include"gui/loop.h"
 #include"gui/utils.h"
 
+#include<stdlib.h>
+#include<string.h>
+
 static bool initialized = false;
 
 static BITMAP *target_bitmap = NULL;
 static int direct_video = 0;
 static unsigned loop_ticks = 100;
+
+typedef struct Nested_s
+{
+  unsigned int cap;
+  unsigned int len;
+  void **a_state;
+  LoopRefresh *a_refresh;
+  LoopLogic *a_logic;
+} Nested;
+static Nested nested_calls = { 0, 0, 0, 0 };
 
 /* Not sure if this is fine in recent machines... */
 static volatile int ticks = 0;
@@ -58,7 +71,7 @@ setup_target_bitmap (void)
 }
 
 void
-loop_ticks_per_second (unsigned i)
+loop_set_ticks_per_second (unsigned i)
 {
   loop_ticks = i;
   if (initialized)
@@ -66,6 +79,11 @@ loop_ticks_per_second (unsigned i)
       if (install_int_ex (&inc_ticks, BPS_TO_TIMER (loop_ticks)) != 0)
 	gui_abort (0);
     }
+}
+unsigned int
+loop_get_ticks_per_second (void)
+{
+  return loop_ticks;
 }
 
 void
@@ -82,10 +100,41 @@ loop_get_direct_video_refresh (void)
   return direct_video;
 }
 
+static void
+refresh_all (BITMAP *target)
+{
+  unsigned int i;
+  for (i = 0; i < nested_calls.len; ++i)
+    nested_calls.a_refresh[i] (target, nested_calls.a_state[i]);
+}
+static LOOPRETURN
+logic_all (void)
+{
+  LOOPRETURN rv;
+  unsigned int i;
+
+  rv = LOOP_CONTINUE;
+  for (i = 0; i < nested_calls.len; ++i)
+    {
+      switch (nested_calls.a_logic[i] (nested_calls.a_state[i]))
+	{
+	case LOOP_CONTINUE:
+	  break;
+	case LOOP_REFRESH:
+	  rv = LOOP_REFRESH;
+	  break;
+	case LOOP_STOP:
+	  if (i == nested_calls.len - 1)
+	    rv = LOOP_STOP;
+	  break;
+	}
+    }
+  return rv;
+}
+
 void
-loop_execute (void *state,
-	      void (*refresh) (BITMAP * buffer, void const *state),
-	      LOOPRETURN (*logic) (void *state))
+loop_subexecute (void *state,
+		 LoopRefresh refresh, LoopInput input, LoopLogic logic)
 {
   bool needs_refresh;
   LOOPRETURN rv;
@@ -93,16 +142,72 @@ loop_execute (void *state,
   if (!initialized)
     {
       initialized = true;
-      loop_ticks_per_second (loop_ticks);
+      LOCK_VARIABLE (ticks);
+      LOCK_FUNCTION (inc_ticks);
+      loop_set_ticks_per_second (loop_ticks);
       setup_target_bitmap ();
     }
 
+  if (nested_calls.len == nested_calls.cap)
+    {
+      unsigned int ncap;
+      void **na_state;
+      LoopRefresh *na_refresh;
+      LoopLogic *na_logic;
+
+      ncap = nested_calls.cap + (nested_calls.cap / 2) + 4;
+
+      na_state = (void **) malloc (ncap * sizeof (void *));
+      if (!na_state)
+	{
+	  gui_abort (0);
+	  abort ();
+	}
+      na_refresh = (LoopRefresh *) malloc (ncap * sizeof (LoopRefresh));
+      if (!na_refresh)
+	{
+	  free (na_state);
+	  gui_abort (0);
+	  abort ();
+	}
+      na_logic = (LoopLogic *) malloc (ncap * sizeof (LoopLogic));
+      if (!na_logic)
+	{
+	  free (na_state);
+	  free (na_refresh);
+	  gui_abort (0);
+	  abort ();
+	}
+
+      memcpy (na_state, nested_calls.a_state,
+	      nested_calls.len * sizeof (void *));
+      memcpy (na_refresh, nested_calls.a_refresh,
+	      nested_calls.len * sizeof (LoopRefresh));
+      memcpy (na_logic, nested_calls.a_logic,
+	      nested_calls.len * sizeof (LoopLogic));
+
+      free (nested_calls.a_state);
+      free (nested_calls.a_refresh);
+      free (nested_calls.a_logic);
+
+      nested_calls.cap = ncap;
+      nested_calls.a_state = na_state;
+      nested_calls.a_refresh = na_refresh;
+      nested_calls.a_logic = na_logic;
+    }
+
+  nested_calls.a_state[nested_calls.len] = state;
+  nested_calls.a_refresh[nested_calls.len] = refresh;
+  nested_calls.a_logic[nested_calls.len] = logic;
+  ++nested_calls.len;
+
   needs_refresh = true;
+  rv = LOOP_CONTINUE;
   do
     {
       if (needs_refresh)
 	{
-	  refresh (target_bitmap, state);
+	  refresh_all (target_bitmap);
 	  if (direct_video)
 	    rest (1);
 	  else
@@ -114,14 +219,34 @@ loop_execute (void *state,
 	  needs_refresh = false;
 	}
 
-      rv = LOOP_CONTINUE;
-      while (ticks > 0 && rv != LOOP_STOP)
+      if (ticks)
 	{
-	  --ticks;
-	  rv = logic (state);
-	  if (rv == LOOP_REFRESH)
-	    needs_refresh = true;
+	  rv = LOOP_CONTINUE;
+	  while (ticks > 0 && rv != LOOP_STOP)
+	    {
+	      --ticks;
+	      input (state);
+	      rv = logic_all ();
+	      if (rv == LOOP_REFRESH)
+		needs_refresh = true;
+	    }
 	}
+      else
+	rest (1);
     }
   while (rv != LOOP_STOP);
+
+  --nested_calls.len;
+}
+
+void
+loop_execute (void *state,
+	      LoopRefresh refresh,
+	      LoopInput input,
+	      LoopLogic logic)
+{
+  Nested cached_nested;
+  cached_nested = nested_calls;
+  loop_subexecute (state, refresh, input, logic);
+  nested_calls = cached_nested;
 }
